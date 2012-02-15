1dfd798f5b247a80cc42a8a64a3732eb2bdb2176
Paul Mitchum <paul@mile23.com>
2012-02-14 22:01:06 -0800

    basic conversion of presidents project to oop for CRUD

--- presidents/presidents.php
+++ presidents/presidents.php
@@ -4,9 +4,15 @@
 <?php
 
 include_once "db.inc";
-include_once "crud.inc";
+include_once "crudoop.inc";
 include_once "sanitize.inc";
 include_once "forms.inc";
+
+// Create a magic db abstraction object.
+// We could put this off until just before we
+// need to query, but for now we're testing
+// CRUDOOP, not efficiency.
+$cruddy = new CRUDOOP(get_db());
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (!empty($_GET['type'])) {
@@ -18,37 +24,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         $id = $_GET['id'];
         if (!is_numeric($id)) $id = -1;
     }
-    form_one_president($id, get_schema());
+    form_one_president($cruddy->load_record($id), get_schema());
   } else {
     // show big form
-    form_all_presidents(crud_president_load_all(), get_schema());
+    form_all_presidents($cruddy->load_all_records(), get_schema());
   }
 } else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-
   // POSTed forms can do one of three things: add, edit, or delete.
   if (!empty($_POST['type'])) {
     $type = $_POST['type']; // save some typing
     switch ($type) {
       case 'delete':
         // delete item
-        crud_president_delete_by_id(sanitize_INT($_POST['id']));
+        $cruddy->delete_record(sanitize_int($_POST['id']));
         break;
       case 'add':
-        $input = sanitize_schema_input($_POST, get_schema());
-        crud_president_insert($input);
-        break;
       case 'edit':
         $input = sanitize_schema_input($_POST, get_schema());
-        crud_president_update($input);
+        if ($type == 'add') unset($input['id']);
+        $cruddy->write_record($input);
         break;
     }
   }
-  // show the big list.
-  form_all_presidents(crud_president_load_all(), get_schema());
+  form_all_presidents($cruddy->load_all_records(), get_schema());
 } else {
   echo "<h2>Unsure what you're requesting.</h2>";
-  form_all_presidents(crud_president_load_all(), get_schema());
+  form_all_presidents($cruddy->load_all_records(), get_schema());
 }
 
 ?>
